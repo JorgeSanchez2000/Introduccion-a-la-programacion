ddb2a5780d5cdec8bc457db311e5f7cda996a0ae
Jorge <jlsanchezj2000@gmail.com>
2023-07-07 15:06:55 -0600

    ejercicio tema 3 realizado

--- Actividad3.java
+++ Actividad3.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Actividad3 {
+public class Actividad3 {
+    public static void main(String[] args) {
+
+        //Parte 1
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        suma (a, b, c);
+    }
+
+    private static void suma(int a, int b, int c) {
+        System.out.println(a + b + c);
+
+    }
 }
--- Coche.java
+++ Coche.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Coche {
+public class Coche {
+    public static void main(String[] args){
+
+        int puertas = 0;
+        int addPuertas= 4;
+
+        miCoche(puertas, addPuertas);
+    }
+
+    private static void miCoche (int puertas, int addPuertas){
+        System.out.println(puertas + addPuertas);
+
+    }
 }
